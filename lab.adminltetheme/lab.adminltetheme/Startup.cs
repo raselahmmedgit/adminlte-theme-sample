f08db33d51f1a79c41543851d47ddae56251629c
Rasel Ahmmed Bappi <raselahmmed@hotmail.com>
2016-08-14 21:02:59 +0600

    first

---
+++ lab.adminltetheme/lab.adminltetheme/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(lab.adminltetheme.Startup))]
+namespace lab.adminltetheme
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
